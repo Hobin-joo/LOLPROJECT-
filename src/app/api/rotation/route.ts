f6228d0d81b1cdd7247c1ad6c3be8f32d4713b3c
Hobin-joo <wnghqls12@naver.com>
2024-12-18 11:27:53 +0900

    .

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -4,23 +4,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const Api = process.env.RIOT_API_KEY!;
-  try {
-    const res = await fetch(
-      "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-      {
-        method: "GET",
-        headers: {
-          "X-Riot-Token": Api,
-        },
-      }
-    );
-    const data: ChampionRotation = await res.json();
-    const champions = await getChampions();
-    const newChampions = champions?.filter((p) =>
-      data.freeChampionIds?.includes(Number(p.key))
-    );
-    return NextResponse.json(newChampions);
-  } catch (error) {
-    console.error(error);
-  }
+  const res = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": Api,
+      },
+    }
+  );
+  const data: ChampionRotation = await res.json();
+  const champions = await getChampions();
+  const newChampions = champions?.filter((p) =>
+    data.freeChampionIds?.includes(Number(p.key))
+  );
+  return NextResponse.json(newChampions);
 }
